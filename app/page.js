950b2c2da2826d1aded81566b7d400ca567b3597
jamato <jmarcus1165@gmail.com>
2023-11-19 20:59:46 -0500

    added contact, reworked home page, and fixed typos

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 'use client'
 import PageTransition from "@/components/page-transition"
-import { Box, Flex, ChakraProvider, Image, Text, Heading, UnorderedList, ListItem, extendTheme, Center, AbsoluteCenter,  } from "@chakra-ui/react"
+import { Box,  Divider, Flex, ChakraProvider, Image,  Heading,  Center,   } from "@chakra-ui/react"
+import {TypeAnimation} from 'react-type-animation'
+
 
 export default function Page() {
   return (
@@ -11,7 +13,7 @@ export default function Page() {
             <Center>
               <Flex id='name-img' alignItems='center' mt='3rem'>
                 <Box id='name' width='fit-content'>
-                  <Heading color='#d3d3d3' size='lg' fontFamily='M PLUS Rounded 1c' className="typewriter">Jack Torrealba</Heading>
+                  <Heading color='#d3d3d3' size='lg' fontFamily='M PLUS Rounded 1c'>Jack Torrealba</Heading>
                 </Box>
                 <Box display='flex' justifyContent='center' alignItems='center'>
                   <Image borderRadius='full' boxSize='100px' src="/torrealbaProfile.jpg" alt="Jack Torrealba" id="profile-pic"/>
@@ -19,12 +21,26 @@ export default function Page() {
               </Flex>
             </Center>
 
-            <Center mt='2rem'>
-              <Text color='#d3d3d3' className="description" fontSize='18px' >
-                I&apos;m a full-stack web developer with a machine learning background.
-              </Text>
+            <Center mt='2rem' color='#d3d3d3' fontSize='22px'>
+              <Box>
+                <TypeAnimation
+                  sequence={[
+                    'Software Developer',
+                    1000,
+                    'Data Scientist',
+                    1000
+                  ]}
+                  repeat={Infinity}
+                  wrapper="span"
+                  deletionSpeed={60}
+                />
+              </Box>
             </Center>
-
+            
+            <Center mt='1rem'>
+              <Divider width='50%'></Divider>
+            </Center>
+            
         </PageTransition>
       </ChakraProvider>
     </>
--- components/navbar.js
+++ components/navbar.js
@@ -1,42 +1,44 @@
 'use client'
-import { Box, Menu, Center, Button, ButtonGroup, ChakraProvider, Heading, extendTheme, ColorModeScript, useColorMode, MenuButton, MenuList, MenuItem } from "@chakra-ui/react"
+import { Box,  Menu, Center, Button,  ChakraProvider, MenuButton, MenuList, MenuItem,  } from "@chakra-ui/react"
 import Link from 'next/link'
 import {FaLinkedin, FaGithub} from 'react-icons/fa'
-import { DownloadIcon, HamburgerIcon } from "@chakra-ui/icons"
+import { DownloadIcon, HamburgerIcon, EmailIcon } from "@chakra-ui/icons"
 import NavSeparator from "./NavSeparator"
 import { IconButton } from "@chakra-ui/react"
 
-export default function NavBar(){
 
+
+
+export default function NavBar(){
     return (
 
         <ChakraProvider>
-            
-
                 <Center className="navbar">
                     <Box className="navbar-box" w='50%' display='flex' justifyContent='space-evenly'>
 
                         <Link className="menu-items" href='/'>Home</Link>
                         <Link className="menu-items" href='/Portfolio'>Portfolio</Link>
                         <Link className="menu-items" href='/AboutMe'>About Me</Link>
-                        
+                        <Link className='menu-items' id="contactMe" href='mailto:jack.torrealba@uconn.edu' size='md' >
+                            Contact Me
+                        </Link>
+                       
                         <Box className="menu-icon">
-                            <Link href='https://www.linkedin.com/in/jack-torrealba-743a90192/' target="_blank" rel="noreferrer">
-                                <FaLinkedin id='linkedInIcon' color='#0077b5' size='30px'></FaLinkedin>
+                            <Link  href='https://www.linkedin.com/in/jack-torrealba-743a90192/' target="_blank" rel="noreferrer">
+                                <FaLinkedin id="linkedInIcon" color='#0077b5' size='30px'></FaLinkedin>
                             </Link>
                         </Box>
                         <Box className="menu-icon">
                             <Link href='https://github.com/jacktorrealba' target="_blank" rel="noreferrer">
-                                <FaGithub  color='#d3d3d3' size='30px'></FaGithub>
+                                <FaGithub id="githubIcon" color='#d3d3d3' size='30px'></FaGithub>
                             </Link>
                         </Box>
-
                         <Box className="menu-icon">
                             <Button size='md' rightIcon={<DownloadIcon></DownloadIcon>}>
                                 <a href="./TorrealbaResume.pdf" download='TorrealbaResume.pdf' target="_blank" rel="noreferrer">Resume</a>
                             </Button>
                         </Box>
-
+                        
                         <Box id="menu-div" display='none'>
                             <Menu>
                                 <MenuButton 
@@ -58,16 +60,16 @@ export default function NavBar(){
                                             Resume <DownloadIcon/>
                                         </Link>
                                     </MenuItem>
+                                    <MenuItem bg='#d3d3d3'>
+                                        <Link className="menu-items-sm" href='mailto:jack.torrealba@uconn.edu'>
+                                            Contact Me <EmailIcon mb='5px'/>
+                                        </Link>
+                                    </MenuItem>
                                 </MenuList>
                             </Menu>
                         </Box>
                     </Box>
                 </Center>
-                
-
-                
-            
-
             <NavSeparator></NavSeparator>
         </ChakraProvider>
     )
